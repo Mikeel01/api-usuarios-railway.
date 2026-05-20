084d07bb44b7b0229d7a487c307448ad16e919f6
Mikeel01 <sina.m.igneel01@gmail.com>
2026-05-20 11:43:00 -0600

    Create conexion.php

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$database = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
+
+$conn = new mysqli($host, $user, $password, $database, $port);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+?>
